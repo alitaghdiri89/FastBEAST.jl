c07c3b5dfb312a47609bd26baded55057b7e98f5
aliat <ali.a.taghdiri@gmail.com>
2025-06-04 16:25:15 +0200

    2nd iteration using block assembler

--- example/hmatriceslib.jl
+++ example/hmatriceslib.jl
@@ -41,3 +41,6 @@ blk
 ##
 #ClusterTree
 ClusterTree(spaceX.pos)
+
+##
+
--- src/hmatricesinterface.jl
+++ src/hmatricesinterface.jl
@@ -1,23 +1,39 @@
 using BEAST
 using CompScienceMeshes
-
-
-function hmatricesinterface(block)
-    function f(row, column)
-        index = (row.-1) * block.size[1] + column
-        return block[index]
-    end
-    """Each element needs to be an SVector because that's what assemble_hmatrix expects"""
-    X = [SVector(i) for i in 1:block.size[1]]
-    Y = [SVector(i) for i in 1:block.size[2]]
-    return KernelMatrix(f,X,Y)
+using HMatrices
+##
+struct InterfaceAbstractMatrix <:AbstractMatrix{Float64}
+    operator::BEAST.HH3DSingleLayerFDBIO
+    space::BEAST.LagrangeBasis
 end
 
+function Base.getindex(K::InterfaceAbstractMatrix, i::Int, j::Int)
+    blkasm = BEAST.blockassembler(K.operator, K.space, K.space)    
+    function blkassembler(Z, tdata, sdata)
+        @views store(v,m,n) = (Z[m,n] += v)
+        blkasm(tdata,sdata,store)
+    end
+    blk = zeros(ComplexF64, 1, 1)
+    blkassembler(blk, [i], [j])
+    return blk[1, 1]
+end
+Base.size(K::InterfaceAbstractMatrix) = length(K.space.pos)
 
 ## 
-"""
-1st test the small 3 * 3 blk matrix, seems to work fine.
-"""
+myMat = InterfaceAbstractMatrix(op, spaceX)
+myMat[1, 1]
+##
+
+function interface_to_hmatrix(operator, space)
+    abstract_matrix = InterfaceAbstractMatrix(operator, space)
+    Xclt = Yclt = ClusterTree(space.pos)
+    h_matrix = assemble_hmatrix(abstract_matrix, Xclt, Yclt)
+    return h_matrix 
+end
+##
+hmat = interface_to_hmatrix(op, spaceX)
+hmat
+##
 Γ = meshsphere(1.0, 0.1) 
 Γ.vertices
 Γ.faces
@@ -25,43 +41,32 @@ op = Helmholtz3D.singlelayer()
 spaceX = lagrangecxd0(Γ)
 #spaceY = lagrangecxd0(Γ)
 A = assemble(op, spaceX, spaceX)
+"""
+Below this line are not important
+****************************************************************************************************
+"""
 ##
+spaceX.fns
+spaceX.geo
+##
+
 blkasm = BEAST.blockassembler(op, spaceX,spaceX)    
 function blkassembler(Z, tdata, sdata)
     @views store(v,m,n) = (Z[m,n] += v)
     blkasm(tdata,sdata,store)
 end
 
-##
-blk = zeros(Float64, 3, 3)
-blkassembler(blk, [1, 2, 3], [1, 2, 3])
-blk
-##
-kernel_mat = hmatricesinterface(blk)
-kernel_mat.X
-##
-H = assemble_hmatrix(kernel_mat;atol=1e-6)
-##
-#ClusterTree
-ClusterTree(spaceX.pos)
 
 ##
-"""
-2nd test with the big 3214*3214 matrix A 
-while conversion to kernel matrix seems to work fine, assemble_hmatrix fails
-    Is there any other difference between A and blk apart from their size?
-"""
+blk = zeros()
 
-Γ = meshsphere(1.0, 0.1) 
-Γ.vertices
-Γ.faces
-op = Helmholtz3D.singlelayer()
-spaceX = lagrangecxd0(Γ)
-#spaceY = lagrangecxd0(Γ)
-A = assemble(op, spaceX, spaceX)
-##
-kernel_mat2 = hmatricesinterface(A)
-kernel_mat2
-##
-H2 = assemble_hmatrix(kernel_mat2; atol=1e-6)
+function element(i, j)
+    blk = zeros(ComplexF64, 1, 1)
+    blkassembler(blk, [i], [j])
+    return blk[1, 1]
+end
 
+element(1,1)
+A
+##
+spaceX
