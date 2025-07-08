cde8777f8580e62766695829e3a2f4072c21e380
JoshuaTetzner <joshua.tetzner@uni-rostock.de>
2025-07-08 16:57:13 +0200

    NminTree updates and formatting
    I switched to splitting as long as more than nmin functions are in
    each cluster and modified the default eta.

--- src/clustertree/boxtree.jl
+++ src/clustertree/boxtree.jl
@@ -2,19 +2,17 @@ using ClusterTrees
 using LinearAlgebra
 
 function create_tree(
-    points::Vector{SVector{D, F}},
-    options::BoxTreeOptions{I}
-) where {D, I, F <: Real}
-
-    hs, ct = NminClusterTrees.NminTrees.boundingbox(points)
-    tree = NminClusterTrees.NminTrees.NminTree(length(points), center=ct, radius=F(sqrt(D))*hs)
+    points::Vector{SVector{D,F}}, options::BoxTreeOptions{I}
+) where {D,I,F<:Real}
+    hs, ct = NminClusterTrees.boundingbox(points)
+    tree = NminClusterTrees.NminTree(length(points); center=ct, radius=F(sqrt(D))*hs)
 
     D == 3 ? nchildren=8 : nchildren=4
-    treeoptions = NminClusterTrees.NminTrees.BoxTreeOptions(dim=D)
+    treeoptions = NminClusterTrees.BoxTreeOptions(; dim=D)
     destination = (options.nmin, options.maxlevel)
     state = (1, ct, F(sqrt(D))*hs, 1, 1, Vector(1:length(points)), points)
 
-    NminClusterTrees.NminTrees.child!(tree, treeoptions, state, destination)
+    NminClusterTrees.child!(tree, treeoptions, state, destination)
 
     return tree
 end
--- src/clustertree/tree.jl
+++ src/clustertree/tree.jl
@@ -6,8 +6,7 @@ struct BoxTreeOptions{I}
     maxlevel::I
 end
 
-function BoxTreeOptions(; nmin=1, maxlevel=10)
-
+function BoxTreeOptions(; nmin=50, maxlevel=50)
     return BoxTreeOptions(nmin, maxlevel)
 end
 
@@ -15,39 +14,33 @@ struct KMeansTreeOptions{I}
     nmin::I
     maxlevel::I
     nchildren::I
-    KMeansSettings::NminClusterTrees.NminTrees.KMeansSettings{I}
+    KMeansSettings::NminClusterTrees.KMeansSettings{I}
 end
 
-function KMeansTreeOptions(; 
-    nmin=1, 
-    maxlevel=10,
-    nchildren=2,
-    KMeansSettings=NminClusterTrees.NminTrees.KMeansSettings()
+function KMeansTreeOptions(;
+    nmin=50, maxlevel=50, nchildren=2, KMeansSettings=NminClusterTrees.KMeansSettings()
 )
-
     return KMeansTreeOptions(nmin, maxlevel, nchildren, KMeansSettings)
 end
 
-
 function isfar(
-    testnode::ClusterTrees.LevelledTrees.HNode{D}, 
+    testnode::ClusterTrees.LevelledTrees.HNode{D},
     trialnode::ClusterTrees.LevelledTrees.HNode{D};
-    η=2.0
-) where D
+    η=1.0,
+) where {D}
     test_center = testnode.node.data.ct
-    test_radius =  testnode.node.data.hs
+    test_radius = testnode.node.data.hs
     trial_center = trialnode.node.data.ct
     trial_radius = trialnode.node.data.hs
     center_dist = norm(test_center - trial_center)
     dist = center_dist - (test_radius + trial_radius)
-    
-    if 2 * max(test_radius, trial_radius) < η * (dist) && dist != 0
+
+    if 2 * max(test_radius, trial_radius) <= η * (dist) && dist != 0
         return true
     else
         return false
     end
 end
-
 
 function listnearfarinteractions(
     block_tree::ClusterTrees.BlockTrees.BlockTree{T},
@@ -56,22 +49,20 @@ function listnearfarinteractions(
     nears::Vector{Tuple{Int,Int}},
     fars::Vector{Vector{Tuple{Int,Int}}},
     level::Int;
-    η=1.5
+    η=1.0,
 ) where {T}
-    isfar(state[1], state[2], η=η) && (push!(fars[level], block); return nothing)
+    isfar(state[1], state[2]; η=η) && (push!(fars[level], block); return nothing)
     !ClusterTrees.haschildren(block_tree, block) && (push!(nears, block); return nothing)
     for chd in ClusterTrees.children(block_tree, block)
         children = ClusterTrees.data(block_tree, chd)
         chd_state = (
-            block_tree.test_cluster.nodes[chd[1]],
-            block_tree.trial_cluster.nodes[chd[2]]
+            block_tree.test_cluster.nodes[chd[1]], block_tree.trial_cluster.nodes[chd[2]]
         )
-        listnearfarinteractions(block_tree, chd, chd_state, nears, fars, level + 1, η=η)
+        listnearfarinteractions(block_tree, chd, chd_state, nears, fars, level + 1; η=η)
     end
 end
 
-
-function computeinteractions(tree::ClusterTrees.BlockTrees.BlockTree{T}; η=1.5) where {T}
+function computeinteractions(tree::ClusterTrees.BlockTrees.BlockTree{T}; η=1.0) where {T}
     nears = Tuple{Int,Int}[]
     num_levels = length(tree.test_cluster.levels)
     fars = [Tuple{Int,Int}[] for l in 1:num_levels]
@@ -80,11 +71,54 @@ function computeinteractions(tree::ClusterTrees.BlockTrees.BlockTree{T}; η=1.5)
     root_level = 1
 
     listnearfarinteractions(
-        tree, ClusterTrees.root(tree), root_state, nears, fars, root_level, η=η
+        tree, ClusterTrees.root(tree), root_state, nears, fars, root_level; η=η
     )
     return nears, fars
 end
 
-function value(tree::NminClusterTrees.NminTrees.NminTree{D}, node::I) where {I, D}
-    return NminClusterTrees.NminTrees.value(tree, node)
+function value(tree::NminClusterTrees.NminTree{D}, node::I) where {I,D}
+    return NminClusterTrees.value(tree, node)
+end
+
+function value(tree::NminClusterTrees.NminTree{D}, nodes::Vector{I}) where {I,D}
+    values = Int[]
+    for node in nodes
+        append!(values, NminClusterTrees.value(tree, node))
+    end
+    return values
+end
+
+function levellink(tree::NminClusterTrees.NminTree{D}; node=root(tree), target=1) where {D}
+    return Iterators.filter(
+        n -> tree.nodes[n].height == target, ClusterTrees.DepthFirstIterator(tree, node)
+    )
+end
+
+function cluster_link(tree::NminClusterTrees.NminTree{D}) where {D}
+    link = Vector{Int}[]
+    for level in eachindex(tree.levels)
+        nodes = Int[]
+        for node in FastBEAST.levellink(tree; target=level - 1)
+            push!(nodes, node)
+        end
+        push!(link, nodes)
+    end
+
+    return link
+end
+
+function child_link(tree::NminClusterTrees.NminTree{D}, node::I) where {I,D}
+    childrange = Tuple{Int,UnitRange{Int}}[]
+    if ClusterTrees.haschildren(tree, node)
+        idxcounter = 1
+        for childidx in ClusterTrees.children(tree, node)
+            push!(
+                childrange,
+                (childidx, idxcounter:(idxcounter + length(value(tree, childidx)) - 1)),
+            )
+            idxcounter += length(value(tree, childidx))
+        end
+    end
+
+    return childrange
 end
