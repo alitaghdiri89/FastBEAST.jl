76f11ad54aa418564a3ea5523c66aa9e381934c7
aliat <ali.a.taghdiri@gmail.com>
2025-07-10 14:20:39 +0200

    ACAFact can now be used in HMatrices, cleanup remains

--- src/hmatricesinterface.jl
+++ src/hmatricesinterface.jl
@@ -6,7 +6,6 @@ using StaticArrays
 using Plots
 using DataStructures
 
-## Data structure
 struct InterfaceAbstractMatrix <:AbstractMatrix{Float64}
     operator::BEAST.HH3DSingleLayerFDBIO
     spaceA::BEAST.LagrangeBasis
@@ -60,7 +59,6 @@ function HMatrices.getblock!(
     end
     permuted_irange = K.rowperm[Vector(irange)]
     permuted_jrange = K.colperm[Vector(jrange)]
-
     K.data.blockassembler(out, permuted_irange, permuted_jrange)
     return out
 end
@@ -77,7 +75,6 @@ function get_hmatrix_memory_size(H)
     while !isempty(stack)
         top = pop!(stack)
         if !isnothing(top.data)
-            print()
             if isa(top.data, HMatrices.RkMatrix)
                 element_count += length(top.data.A) + length(top.data.B)
             else
@@ -92,19 +89,15 @@ function get_hmatrix_memory_size(H)
 end 
 # The main interface function
 
-function interface_to_hmatrix(operator, spaceA, spaceB; rtol=1e-6, splitter_nmax=50)
+function interface_to_hmatrix(operator, spaceA, spaceB; rtol=1e-6, splitter_nmax=50, comp=nothing)
     abstract_matrix = InterfaceAbstractMatrix(operator, spaceA, spaceB)
     Xclt = ClusterTree(spaceA.pos, HMatrices.GeometricSplitter(splitter_nmax))
     Yclt = ClusterTree(spaceB.pos, HMatrices.GeometricSplitter(splitter_nmax))
-    h_matrix = assemble_hmatrix(abstract_matrix, Xclt, Yclt, comp = PartialACA(; rtol=rtol), threads =false)
+    comp = isnothing(comp) ? PartialACA(; rtol=rtol) : comp 
+    h_matrix = assemble_hmatrix(abstract_matrix, Xclt, Yclt, comp=comp, threads =false)
     return h_matrix 
 end
 
-##
-@show hmat
-@show get_hmatrix_memory_size(hmat)
-@show Base.summarysize(hmat)
-# @show Base.summarysize(hmat)
 ##
 ## Testing
 # setting up test example sephirical same space
@@ -121,13 +114,13 @@ hmat
 # test example, 2 rectangles one translated
 op = Helmholtz3D.singlelayer()
 Γa = meshrectangle(1.0, 1.0, 0.2)
-Γb = CompScienceMeshes.translate(meshrectangle(1.0, 1.0, 0.2), SVector(3.0, 0.0, 0.0))
+Γb = CompScienceMeshes.translate(meshrectangle(1.0, 1.0, 0.02), SVector(3.0, 0.0, 0.0))
 spaceA = lagrangecxd0(Γa)
 spaceB = lagrangecxd0(Γb)
 dim = length(spaceB.pos)
 
-@time A = assemble(op, spaceA, spaceB)
-@time hmat = interface_to_hmatrix(op, spaceA, spaceB; rtol=1e-4)
+#ä@time A = assemble(op, spaceA, spaceB)
+hmat = interface_to_hmatrix(op, spaceA, spaceB; rtol=1e-6)
 ## 
 println("comparing matrices themselves")
 absolute_diff = norm(Matrix(hmat) - A)
@@ -270,6 +263,7 @@ compratio = [1.004475, 2.410772, 6.490204, 19.156679]
 matdim =[804, 3214, 12526, 48192]
 hsize = [5004528, 33523608, 190538440, 958363176]
 fullsize = [5171328, 82638368, 1255205408, 18579750912]
+myacatime = [0.428401, 2.977444, 15.888848, 76.397884]
 nlogn = [n * log2(n) for n in matdim]
 ## 
 plot(matdim, compratio)
@@ -284,10 +278,12 @@ plot(matdim, fulltime, label="full matrix")
 scatter!(matdim, fulltime, label=false)
 plot!(matdim, htime, label="H matrix")
 scatter!(matdim, htime, label=false)
+plot!(matdim, myacatime, label="myACA")
+scatter!(matdim, myacatime, label=false)
 plot!(legend=true, size=(800, 600), minorgrid=true)
 xlabel!("Matrix dimension")
 ylabel!("time (S)")
-png("time_vs_dim")
+png("time_vs_dim_aca")
 ##
 plot(matdim, rel_errs)
 scatter!(matdim, rel_errs)
@@ -313,43 +309,74 @@ png("memory_vs_dim")
 
 
 ## Playground
+
+struct LeafBlock <:AbstractMatrix{Float64}
+    fullMat :: InterfaceAbstractMatrix
+    irange :: Vector{Int64}
+    jrange :: Vector{Int64}
+end
+
+function LeafBlock(K :: HMatrices.PermutedMatrix, irange :: UnitRange{Int64}, jrange :: UnitRange{Int64})
+    permuted_irange = K.rowperm[Vector(irange)]
+    permuted_jrange = K.colperm[Vector(jrange)]
+    return LeafBlock(K.data, permuted_irange, permuted_jrange)
+end
+
+# Base.getindex(K::LeafBlock, i::Int, j::Int) = K.fullMat[K.irange[i], K.jrange[j]]
+function Base.getindex(K::LeafBlock, i::Int, j::Int)
+    return K.fullMat[K.irange[i], K.jrange[j]]
+end
+Base.size(K::LeafBlock) = length(K.irange), length(K.jrange)
+
 using ACAFact
-log2(4)
-pts1 = range(0.0, 1.0, length=100)
-pts2 = range(0.0, 1.0, length=110)
-K    = [exp(-abs2(pj - pk)) for pj in pts1, pk in pts2]
-@show rank(K) 
-@show K.size
-for tol in (1e-4, 1e-6, 1e-8, 1e-10, 1e-12, 1e-14)
-  (U, V) = aca(K, tol)
-  @show (tol, size(U, 2), opnorm(K - U*V'))
-end
-function myACA()
-end
-##
 Base.@kwdef struct myACA
     atol::Float64 = 0
     rank::Int = typemax(Int)
     rtol::Float64 = atol > 0 || rank < typemax(Int) ? 0 : sqrt(eps(Float64))
 end
 
-##
+
 function (paca::myACA)(K, rowtree::ClusterTree, coltree::ClusterTree, bufs = nothing)
-    # find initial column pivot for partial ACA
-    istart = _aca_partial_initial_pivot(rowtree)
-    irange = index_range(rowtree)
-    jrange = index_range(coltree)
-    return _aca_partial(
-        K,
-        irange,
-        jrange,
-        paca.atol,
-        paca.rank,
-        paca.rtol,
-        istart - irange.start + 1,
-        bufs,
-    )
+    irange = HMatrices.index_range(rowtree)
+    jrange = HMatrices.index_range(coltree)
+    leaf = LeafBlock(K, irange, jrange)
+    (U, V) = aca(leaf, paca.rtol)
+    return HMatrices.RkMatrix(U, V)
+end
+
+function ACAFact.row!(buff, K::LeafBlock, i)
+    reshaped_buff = reshape(buff, (1, size(K, 2)))
+    K.fullMat.blockassembler(reshaped_buff, [K.irange[i]], K.jrange)
+end
+function ACAFact.col!(buff, K::LeafBlock, j)
+    K.fullMat.blockassembler(buff, K.irange, [K.jrange[j]])
 end
 ##
-clt = ClusterTree(spaceX.pos, HMatrices.GeometricSplitter(50))
-HMatrices.index_range(clt)
+powers = collect(-4:-1:-10)
+rtols = [10.0^i for i in powers]
+rel_errors = zeros(Float64, size(rtols))
+
+Γ = meshsphere(1.0, 0.025) 
+op = Helmholtz3D.singlelayer()
+spaceX = lagrangecxd0(Γ)
+dim = length(spaceX.pos)
+##
+A = assemble(op, spaceX, spaceX)
+##
+@time hmat = interface_to_hmatrix(op, spaceX, spaceX; comp=myACA(rtol=1e-6))
+##
+for (i, rtol) in enumerate(rtols)
+    hmat = interface_to_hmatrix(op, spaceX, spaceX; comp=myACA(rtol=rtol))
+    vec = rand(dim)
+    rel_errors[i] = norm(hmat * vec - A * vec) / norm(A * vec) 
+end
+##
+plot(rtols, rel_errors)
+scatter!(rtols, rel_errors)
+plot!(xscale=:log10, yscale=:log10, minorgrid=true)
+plot!(legend=false, size=(800, 600))
+xlabel!("relative tolerance")
+ylabel!("relative error")
+png("err_vs_tol_myaca")
+
+##
